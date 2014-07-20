c1f877eac3f5b9a7ef4af05712193fe4961fa5d1
Jit Kanetkar <kanetkarster@gmail.com>
2014-07-19 22:38:45 -0400

    improved the map loading system

--- public/javascripts/leaflet-embed.js
+++ public/javascripts/leaflet-embed.js
@@ -1,27 +1,28 @@
 var map;
 
+//Shows the GPX file on the map
 function loadGPX(fn) {
-  var gpxFile = typeof fn !== 'undefined'
-                       ? 'gpx/' + fn
-                       : 'gpx/1744902.gpx';
+  var gpxFile = 'gpx/'+fn;
   new L.GPX(gpxFile, {async: true}).on('loaded', function(e) {
     map.fitBounds(e.target.getBounds());
   }).addTo(map);
 }
 
+//Creates the Leaflet Map
 $(document).ready(function(){
   var url = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
   var copyright = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
 
   map = new L.Map('map');
-  map.setView([41.5, -73], 10);
   L.tileLayer(url, {
       attribution: copyright,
       maxZoom: 18
   }).addTo(map);
-
-  loadGPX();
+  //Loads the first GPX file onto the Leaflet Map
+  loadGPX($('#gpxelm').first().html());
 });
+
+//Click Handling on the List
 $(function(){
   $('#gpxlist').on('click', 'li', function(){
     loadGPX($(this).html());
