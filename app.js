a796220c8ec35a45e2d1ca9e020bbc446b6f822e
Jit <kanetkarster@gmail.com>
2014-08-17 15:10:17 -0400

    minor changes, attempting to deploy

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/bike', routes);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var readdirSync = require('fs').readdirSync;
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Mapper', files: readdirSync('../public/gpx') });
+  res.render('index', { title: 'Mapper', files: readdirSync('./public/gpx') });
 });
 
 module.exports = router;
